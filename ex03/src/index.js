2cdd94b75aa58b780e080f31989fb84caa674936
dino-ha <hadzirovicdino@gmail.com>
2022-04-04 23:25:38 +0200

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,20 @@
+localScope();
 
+if (typeof myVariable != "undefined") {
+
+    console.log('outside localScope', myVariable)
+} else {
+
+    console.log('outside localScope UNDEFINED!!!')
+    }
+
+
+function localScope(){
+
+    var myVariable = "I am here!";
+    console.log(myVariable);
+    return;
+    
+}
+
+module.exports = localScope;
