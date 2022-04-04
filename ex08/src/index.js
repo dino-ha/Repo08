031c5daf47d304127895ff475bedc6a9412e55e2
dino-ha <hadzirovicdino@gmail.com>
2022-04-04 23:28:40 +0200

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,12 @@
+var total = 0;
 
+function funcWithArg(digit){
+
+    var digit = (digit + 9)/5;
+    return digit;
+}
+
+total = funcWithArg(6);
+console.log(total);
+
+module.exports = funcWithArg;
