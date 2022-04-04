93d67d1f50d4dbbfde7facc84ab1f40fe2abf765
dino-ha <hadzirovicdino@gmail.com>
2022-04-04 23:26:41 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,23 @@
+var sum = 0;
 
+function addThree(){
+
+    sum = sum + 3;
+    console.log(sum);
+}
+
+function addFive(){
+
+    sum = sum + 5;
+    console.log(sum);
+}
+
+addThree();
+addFive();
+
+
+module.exports = {
+
+    addThree,
+    addFive
+};
